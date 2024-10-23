1b3b97f21331674650fc7818c4b820aee5ffe614
Dmitry Fuks <d.fuks@dodobrands.io>
2024-10-23 22:09:15 +0300

    nvmrc, 22 node

--- eslint.config.js
+++ eslint.config.js
@@ -4,17 +4,12 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(eslintPluginPrettierRecommended, {
+	files: ['**/*.ts', '**/*.ts', '**/*.vue'],
 	rules: {
 		'@typescript-eslint/explicit-function-return-type': [
 			`error`,
 			{
 				allowExpressions: true,
-			},
-		],
-		'vue/multi-word-component-names': [
-			'error',
-			{
-				ignores: ['default'],
 			},
 		],
 	},
